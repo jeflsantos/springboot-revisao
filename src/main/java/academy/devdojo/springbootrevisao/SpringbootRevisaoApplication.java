9e66241d6a4dccbcebd2c888a11a9f2b7cf313ee
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-10-18 18:58:31 -0300

    .

---
+++ src/main/java/academy/devdojo/springbootrevisao/SpringbootRevisaoApplication.java
@@ -0,0 +1,13 @@
+package academy.devdojo.springbootrevisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRevisaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRevisaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/academy/devdojo/springbootrevisao/SpringbootRevisaoApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.devdojo.springbootrevisao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRevisaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

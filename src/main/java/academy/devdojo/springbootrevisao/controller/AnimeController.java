090cc30a8f441d2682ad37730b5f9676c3d7c5b3
Jefferson Luis <69135200+jeflsantos@users.noreply.github.com>
2022-10-19 23:17:20 -0300

    Spring Boot Revisão

--- src/main/java/academy/devdojo/springbootrevisao/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springbootrevisao/controller/AnimeController.java
@@ -5,8 +5,12 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +38,24 @@ public class AnimeController {
 	@GetMapping(path = "/{id}")
 	public ResponseEntity<Anime> findById(@PathVariable long id){
 		return ResponseEntity.ok(animeService.findById(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Anime> save(@RequestBody Anime anime){
+		return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+		
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable long id){
+		animeService.delete(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Void> replace(@RequestBody Anime anime){
+		animeService.replace(anime);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 	
 	
--- src/main/java/academy/devdojo/springbootrevisao/service/AnimeService.java
+++ src/main/java/academy/devdojo/springbootrevisao/service/AnimeService.java
@@ -1,6 +1,8 @@
 package academy.devdojo.springbootrevisao.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -11,7 +13,11 @@ import academy.devdojo.springbootrevisao.domain.Anime;
 @Service
 public class AnimeService {
 	
-	private List<Anime> animes = List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk"));
+	private static List<Anime> animes;
+	
+	static {
+		animes = new ArrayList<>(List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk")));
+	}
 
 	public List<Anime> listAll(){
 		return animes;
@@ -22,5 +28,22 @@ public class AnimeService {
 				.filter(anime -> anime.getId().equals(id))
 				.findFirst()
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found!"));
+	}
+
+	public Anime save(Anime anime) {
+		anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+		animes.add(anime);
+		return anime;
+	}
+
+	public void delete(long id) {
+		animes.remove(findById(id));
+		
+	}
+
+	public void replace(Anime anime) {
+		delete(anime.getId());
+		animes.add(anime);
+		
 	}
 }
